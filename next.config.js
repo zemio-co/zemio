677a8dbea385298fc3f7daf31293648a369b98bb
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2026-04-14 15:11:56 +0200

    fix: reduce monitoring data collection

--- next.config.js
+++ next.config.js
@@ -28,19 +28,7 @@ export default withSentryConfig(config, {
 	// Upload a larger set of source maps for prettier stack traces (increases build time)
 	widenClientFileUpload: true,
 
-	// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-	// This can increase your server load as well as your hosting bill.
-	// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-	// side errors will fail.
-	tunnelRoute: "/monitoring",
-
 	webpack: {
-		// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-		// See the following for more information:
-		// https://docs.sentry.io/product/crons/
-		// https://vercel.com/docs/cron-jobs
-		automaticVercelMonitors: true,
-
 		// Tree-shaking options for reducing bundle size
 		treeshake: {
 			// Automatically tree-shake Sentry logger statements to reduce bundle size
