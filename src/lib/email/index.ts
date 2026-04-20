33094366f0f5cc6f3208226e6815356a1af02ecd
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2026-04-20 12:56:26 +0200

    feat: make resend the only email adapter

--- src/lib/email/index.ts
+++ src/lib/email/index.ts
@@ -1,15 +1,9 @@
 import { env } from "@/env";
 import { createResendAdapter } from "./adapters/resend";
-import { createSMTPAdapter } from "./adapters/smtp";
 import type { EmailAdapter } from "./types/adapter";
 
 function createMailer(): EmailAdapter {
-	switch (env.MAIL_ADAPTER) {
-		case "resend":
-			return createResendAdapter();
-		case "smtp":
-			return createSMTPAdapter();
-	}
+	return createResendAdapter();
 }
 
 const globalForEmail = globalThis as unknown as {
