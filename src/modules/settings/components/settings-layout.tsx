f5fcf0fdb5a0ca25db2e3e250171e4620d7afbc4
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2026-04-21 13:27:18 +0200

    fix: make settings accessible on mobile

--- src/modules/settings/components/settings-layout.tsx
+++ src/modules/settings/components/settings-layout.tsx
@@ -4,9 +4,9 @@ import { SettingsSidebar } from "./settings-sidebar";
 function SettingsLayout({ children }: React.PropsWithChildren) {
 	return (
 		<main className="min-h-svh bg-zinc-50 py-20">
-			<div className="mx-auto flex w-full max-w-6xl gap-8 px-8">
-				<SettingsSidebar className="w-full max-w-64 shrink-0" />
-				<div className="grow">{children}</div>
+			<div className="mx-auto grid w-full max-w-6xl gap-12 px-8 lg:grid-cols-4 lg:gap-8">
+				<SettingsSidebar />
+				<div className="px-2.5 lg:col-span-3 lg:px-0">{children}</div>
 			</div>
 		</main>
 	);
