e08c894a5dbf199a88b376890b1ad4d79c99ed95
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2026-06-11 16:17:53 +0200

    feat(web): improve textarea and input component

--- apps/web/src/app/(app)/components/button/page.tsx
+++ apps/web/src/app/(app)/components/button/page.tsx
@@ -1,5 +1,13 @@
 import { ArrowRightIcon, PencilIcon, PlusIcon, TrashIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+	InputGroup,
+	InputGroupAddon,
+	InputGroupInput,
+	InputGroupText,
+} from "@/components/ui/input-group";
+import { Textarea } from "@/components/ui/textarea";
 
 export default async function ServerPage() {
 	return (
@@ -27,6 +35,16 @@ export default async function ServerPage() {
 				<Button size={"sm"} variant={"outline"}>
 					Bearbeiten
 				</Button>
+			</div>
+			<div className="mt-20">
+				<Input placeholder="Hello world" />
+				<Textarea className="my-12" placeholder="Type something here..." />
+				<InputGroup>
+					<InputGroupInput placeholder="hello world" />
+					<InputGroupAddon>
+						<InputGroupText>Hello world</InputGroupText>
+					</InputGroupAddon>
+				</InputGroup>
 			</div>
 		</main>
 	);
